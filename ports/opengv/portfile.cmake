d33977ac708802cc588da6b2e2ee3f959f454cc1
Zell <m.naghizadeh91@gmail.com>
2022-10-05 01:06:09 +0200

    update opengv actual hash

--- ports/opengv/portfile.cmake
+++ ports/opengv/portfile.cmake
@@ -4,7 +4,7 @@ set(opengv_VERSION 1.0)
 vcpkg_download_distfile(ARCHIVE
     URLS "https://github.com/laurentkneip/opengv/archive/master.zip"
     FILENAME "opengv-master.zip"
-    SHA512 6379666ef57af4ea85026644fa21365ce18fbaa12d50bd452bcdae0743a7b013effdd42c961e90c31815991bf315bd6904553dcc1a382ff5ed8c7abe9edf9a6c
+    SHA512 06e38dafe31e35037695ca3ce0a07296b3adcb079b08115c113135788f0047de0bb10ee51adbd5595d17e934cacafa6498b954584e07b721b5da8c4decd2ab3b
 )
 
 vcpkg_extract_source_archive_ex(
